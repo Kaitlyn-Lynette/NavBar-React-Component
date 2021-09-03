f44ff0db03b945958fe42bf761790900e3b13012
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-09-03 10:42:44 -0400

    The links have a padding of 22 when in mobile but I want them right aligned on desktop

--- src/components/NavExample.js
+++ src/components/NavExample.js
@@ -28,17 +28,22 @@ const NavExample = () => {
   });
 
   return (
-    <Navbar collapseOnSelect expand="lg" className={navbar ? "nav-active" : "nav-bar"}>
-        <Navbar.Brand href="#home" className="nav-brand">{name}</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto nav-tabs">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className={navbar ? "nav-active" : "navbar"}
+    >
+      <Navbar.Brand href="#home" className="nav-brand">
+        {name}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="nav-links">
             <Nav.Link href="#pageone">{navOne}</Nav.Link>
             <Nav.Link href="#pagetwo">{navTwo}</Nav.Link>
             <Nav.Link href="#pagethree">{navThree}</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
-  
+      </Navbar.Collapse>
     </Navbar>
   );
 };
